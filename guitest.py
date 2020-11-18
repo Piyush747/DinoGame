a7fae7274d8de86bd30ef82ee965006fbee77fca
Piyush747 <74671725+Piyush747@users.noreply.github.com>
2020-11-18 21:50:37 +0530

    Update guitest.py

--- guitest.py
+++ guitest.py
@@ -1,39 +1,37 @@
 import pyautogui
 from PIL import Image, ImageGrab
 import time
-#from numpy import asarray
-def hit(key):
+def hit(key): #function to automate keyboard
     pyautogui.press(key)
     return
-#def draw():
-def isCollide(data):
+def isCollide(data): #function to check for cactus and birds 
     #check for birds
     for i in range(210,250):
         for j in range(300,380):
             if data[i,j] < 100:
-                hit("down")
+                hit("down") #if a bird is detected ahead of dino down key is pressed
     #check for cactus
     for i in range(220,330):
         for j in range(455,500):
             if data[i,j] < 100:  #0 means black and 255 means white
-                hit("up")
+                hit("up") ##if a cactus is detected ahead of dino up key is pressed
                 return 
     return 
 
 if __name__=='__main__':
-    print("Hey Dino game is about to start in 3 seconds")
-    time.sleep(2)
-    hit("up")
+    print("Hey Dino game is about to start in 2 seconds")
+    time.sleep(2) # wait for 2 seconds
+    hit("up") # start the dino game
     while True:
         image = ImageGrab.grab().convert('L')  # takes ss and converts image to greyscale
         
         data = image.load()
         isCollide(data)
-        #print(asarray(image))
 
-        #draw the rectangle for cactus
-        
+        #documentation to set the space to detect cactus and birds
+        #set it according to screen resolution by hit and trail method
     '''
+        #draw the rectangle for cactus
         for i in range(220,300):
             for j in range(455,500):
                 data[i,j]=0
